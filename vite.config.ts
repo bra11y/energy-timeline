04bd3e6fd508fbe4b7c35dce7871cfdcd5ec1e34
bra11y <bryanonyen.inc@gmail.com>
2025-07-30 19:25:19 +0100

    fix: tailwind on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const isGitHubPages = mode === 'github' || process.env.GITHUB_ACTIONS === 'true'
   
   return {
     plugins: [
-      react(),
-      tailwindcss()
+      react()
     ],
     // Set correct base path for GitHub Pages
     base: isGitHubPages ? '/energy-timeline/' : '/',
